54bcc6ec1457b7ba7fc5998d7781b9a4e0bb67d9
bhavy rahangdale <114098067+bhavy2202@users.noreply.github.com>
2023-06-06 16:21:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 5500
 
 const USERS = [];
 
@@ -21,30 +21,43 @@ const SUBMISSION = [
 app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
+  const { email, password } = req.body;
 
+  // Store email and password (as is for now) in the USERS array above (only if the user with the given email doesn't exist)
+  const existingUser = USERS.find(user => user.email === email);
+  if (existingUser) {
+    return res.status(400).json({ error: 'User with the provided email already exists' });
+  }
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  USERS.push({ email, password });
 
+  // Return back 200 status code to the client
+  return res.sendStatus(200);
+});
 
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
 
 app.post('/login', function(req, res) {
-  // Add logic to decode body
+ // Add logic to decode body
   // body should have email and password
+  const { email, password } = req.body;
 
   // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  const user = USERS.find(user => user.email === email);
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
 
+  // Ensure that the password is the same
+  if (user.password !== password) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  // If the password is the same, generate a token (any random string will do for now)
+  const token = generateToken();
 
-
-  res.send('Hello World from route 2!')
-})
+  // Return back 200 status code and the token to the client
+  return res.status(200).json({ token });
+});
 
 app.get('/questions', function(req, res) {
 
@@ -53,21 +66,40 @@ app.get('/questions', function(req, res) {
 })
 
 app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  // Return the user's submissions for this problem
+  const userEmail = req.query.email; // Assuming the email is passed as a query parameter
+
+  // Find the user in the USERS array
+  const user = USERS.find(user => user.email === userEmail);
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  // Retrieve the user's submissions for this problem (assuming it's stored in the user object)
+  const submissions = user.submissions; // Adjust this based on your data structure
+
+  // Return the submissions to the client
+  return res.status(200).json(submissions);
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
-});
-
-// leaving as hard todos
 // Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+app.post('/problems', function(req, res) {
+  // Check if the user is an admin
+  const isAdmin = req.headers.authorization === 'Bearer ADMIN_TOKEN'; 
+  if (!isAdmin) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
 
+  // Add logic to handle the creation of a new problem
+  // ...
+  // ...
+
+  // Return a success message or appropriate response to the client
+  return res.status(200).json({ message: 'Problem created successfully' });
+});
+
+// Set up the server to listen on the specified port
 app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
